41c47399181d0ca23ae238e2bb8b791d0002b31e
Bruno Rogério <b.rogerio2007@gmail.com>
2021-07-12 18:09:21 -0400

    Salvando aprendizado no Git

---
+++ src/aprendizadoAvancadoJava/lambda/function/Manual.java
@@ -0,0 +1,11 @@
+package aprendizadoAvancadoJava.lambda.function;
+
+public class Manual {
+
+    public static void main(String[] args) {
+
+        // Recebe um tipo e retorna um outro tipo
+
+
+    }
+}
