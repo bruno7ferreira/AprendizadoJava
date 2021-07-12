40225458fd7a8a40389bd4029401e13012691a4f
Bruno Rogério <b.rogerio2007@gmail.com>
2021-07-12 19:15:06 -0400

    Salvando aprendizado no Git

--- src/aprendizadoAvancadoJava/lambda/function/Manual.java
+++ src/aprendizadoAvancadoJava/lambda/function/Manual.java
@@ -1,11 +1,26 @@
 package aprendizadoAvancadoJava.lambda.function;
 
+import java.util.function.Function;
+
 public class Manual {
 
     public static void main(String[] args) {
 
-        // Recebe um tipo e retorna um outro tipo
+        // Recebe dois tipos e retorna um outro tipo
 
+        Function<Integer, String> parOuImpar = n -> n % 2 == 0 ? "Par" : "Ímpar";
+
+        System.out.println(parOuImpar.apply(27));
+
+        Function<String, String> oResutladoE = v -> "O resultado é: " + v;
+
+        Function<String, String> empolgado = x -> x + "!!";
+
+        String resultadoFinal = parOuImpar
+                .andThen(oResutladoE) //Função encadeada
+                .andThen(empolgado) //Função encadeada
+                .apply(27);
+        System.out.println(resultadoFinal);
 
     }
 }
