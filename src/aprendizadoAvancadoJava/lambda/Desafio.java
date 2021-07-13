f44cb408ab2fa77a343e79292e8fcb4bd1d5b6c4
Bruno Rogério <b.rogerio2007@gmail.com>
2021-07-13 17:23:49 -0400

    Salvando aprendizado no Git

--- src/aprendizadoAvancadoJava/lambda/Desafio.java
+++ src/aprendizadoAvancadoJava/lambda/Desafio.java
@@ -1,10 +1,14 @@
 package aprendizadoAvancadoJava.lambda;
 
+import java.util.function.Function;
+
 public class Desafio {
 
     public static void main(String[] args) {
 
         Produto p = new Produto("Notebook", 3000.00, 0.13);
+
+        Function<Produto, Double> precoFinal;
 
     }
 
