b802544f6edefe1214f685bdea624f3110c51955
Bruno Rogério <b.rogerio2007@gmail.com>
2021-07-14 20:08:50 -0400

    Salvando aprendizado no Git

--- src/aprendizadoAvancadoJava/lambda/Desafio.java
+++ src/aprendizadoAvancadoJava/lambda/Desafio.java
@@ -1,14 +1,34 @@
 package aprendizadoAvancadoJava.lambda;
 
+import java.util.Locale;
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class Desafio {
 
     public static void main(String[] args) {
 
+
+        Function<Produto, Double> precoFinal = finalPrice -> finalPrice.preco * (1 - finalPrice.desconto);
+        UnaryOperator<Double> impostoMunipal = cityTax -> cityTax >= 2500 ? cityTax * 1.085 : cityTax;
+        UnaryOperator<Double> frete = shipping -> shipping >= 3000 ? shipping + 100 : shipping + 50;
+        Function<Double, String> arredondar = round -> String.valueOf(String.format("%.2f", round))
+                .replace(".", ",");
+        //Function<Double, String> formatar = format -> ("R$" + format).replace(".", ",");
+
         Produto p = new Produto("Notebook", 3000.00, 0.13);
 
-        Function<Produto, Double> precoFinal;
+        String preco = precoFinal
+                .andThen(impostoMunipal)
+                .andThen(frete)
+                .andThen(arredondar)
+                //.andThen(formatar)
+                .apply(p);
+
+        System.out.println("O preco final é: " + preco);
+
+
+
 
     }
 
--- src/aprendizadoAvancadoJava/lambda/predicate/Manual.java
+++ src/aprendizadoAvancadoJava/lambda/predicate/Manual.java
@@ -1,5 +1,7 @@
 package aprendizadoAvancadoJava.lambda.predicate;
 
+import aprendizadoAvancadoJava.lambda.Produto;
+
 import java.util.function.Predicate;
 
 public class Manual {
@@ -8,7 +10,7 @@ public class Manual {
 
         // Representa uma função que recebe um tipo retorna um valor verdadeiro ou falso
 
-        Predicate<Produto> isCaro = prod -> (prod.preco * (1-prod.desconto))  >= 800;
+        Predicate<Produto> isCaro = prod -> (prod.getPreco() * (1 - prod.getDesconto())) >= 800;
 
         Produto produto = new Produto("Notebook", 4000, 0.15);
         System.out.println(isCaro.test(produto));
