3a45fbd0e75b14d32223fead17418c3587cbb413
Bruno Rogério <b.rogerio2007@gmail.com>
2021-07-17 20:09:39 -0400

    Salvando aprendizado no Git

---
+++ src/aprendizadoAvancadoJava/stream_api/Aluno.java
@@ -0,0 +1,26 @@
+package aprendizadoAvancadoJava.stream_api;
+
+public class Aluno {
+
+    final private String nome;
+    final private double nota;
+
+    public Aluno(String nome, double nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    @Override
+    public String toString() {
+        return "nome= " + nome +
+                ", nota= " + nota;
+    }
+}
--- src/aprendizadoAvancadoJava/stream_api/filter/Filter.java
+++ src/aprendizadoAvancadoJava/stream_api/filter/Filter.java
@@ -1,8 +1,33 @@
 package aprendizadoAvancadoJava.stream_api.filter;
 
+import aprendizadoAvancadoJava.stream_api.Aluno;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class Filter {
 
     public static void main(String[] args) {
+
+        Consumer<Aluno> imprimir = System.out::println;
+
+        Aluno a1 = new Aluno("Bruno", 9);
+        Aluno a2 = new Aluno("Rogério", 6.7);
+        Aluno a3 = new Aluno("Ferreira", 8);
+        Aluno a4 = new Aluno("Alexandra", 10);
+        Aluno a5 = new Aluno("Maria Clara", 10);
+        Aluno a6 = new Aluno("Paulo", 8);
+
+        List<Aluno> claseDeAluno = new ArrayList<>();
+        claseDeAluno.add(a1);
+        claseDeAluno.add(a2);
+        claseDeAluno.add(a3);
+        claseDeAluno.add(a4);
+        claseDeAluno.add(a5);
+        claseDeAluno.add(a6);
+
+        claseDeAluno.forEach(imprimir);
 
 
     }
