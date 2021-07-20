1eccc2aa3bcb23be3e17117681bdfbdaf18c383a
Bruno Rogério <b.rogerio2007@gmail.com>
2021-07-20 19:26:50 -0400

    Salvando aprendizado no Git

---
+++ src/aprendizadoAvancadoJava/stream_api/reduce/Reduce.java
@@ -0,0 +1,30 @@
+package aprendizadoAvancadoJava.stream_api.reduce;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+public class Reduce {
+
+    public static void main(String[] args) {
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        BinaryOperator<Integer> soma = (cont, x) -> cont + x;
+
+
+        Integer total = numeros.stream()
+                .reduce(soma)
+                .get();//Reduce é uma operação terminal
+
+        System.out.println(total);
+
+        Integer total2 = numeros.stream()
+                .reduce(100, soma);
+        System.out.println(total2);
+
+
+    }
+
+}
