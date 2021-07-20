f1e925938b1ffb7616387770bfe6052049795173
Bruno Rogério <b.rogerio2007@gmail.com>
2021-07-19 22:11:19 -0400

    Salvando aprendizado no Git

---
+++ src/aprendizadoAvancadoJava/stream_api/Produto.java
@@ -0,0 +1,16 @@
+package aprendizadoAvancadoJava.stream_api;
+
+public class Produto {
+
+    private String nome;
+    private double preco;
+    private double desconto;
+    private double valorFrete;
+
+    public Produto(String nome, double preco, double desconto, double valorFrete) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+        this.valorFrete = valorFrete;
+    }
+}
