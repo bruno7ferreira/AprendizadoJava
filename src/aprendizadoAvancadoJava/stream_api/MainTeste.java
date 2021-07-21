508d2b7f9297a447a5e068fd81ad97c1ed8ceba7
Bruno Rogério <b.rogerio2007@gmail.com>
2021-07-21 19:43:59 -0400

    Salvando aprendizado no Git

--- src/aprendizadoAvancadoJava/stream_api/MainTeste.java
+++ src/aprendizadoAvancadoJava/stream_api/MainTeste.java
@@ -4,10 +4,12 @@ public class MainTeste {
 
     public static void main(String[] args) {
 
-        Media m1 = new Media();
-        m1.adicionar(8.3);
-        m1.adicionar(6.7);
+        Media m1 = new Media().adicionar(8.3).adicionar(6.7);
+        Media m2 = new Media().adicionar(7.9).adicionar(6.6);
         System.out.println(m1.getValue());
+        System.out.println(m2.getValue());
+
+        System.out.println(Media.combinar(m1, m2).getValue());
 
 
     }
---
+++ src/aprendizadoAvancadoJava/stream_api/reduce/Reduce3.java
@@ -0,0 +1,37 @@
+package aprendizadoAvancadoJava.stream_api.reduce;
+
+import aprendizadoAvancadoJava.stream_api.Aluno;
+import aprendizadoAvancadoJava.stream_api.Media;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Reduce3 {
+
+    public static void main(String[] args) {
+
+        Aluno a1 = new Aluno("Bruno", 7.1);
+        Aluno a2 = new Aluno("Rogério", 6.1);
+        Aluno a3 = new Aluno("Ferreira", 8.1);
+        Aluno a4 = new Aluno("Kobe", 9.0);
+
+        List<Aluno> listaAlunos = Arrays.asList(a1, a2, a3, a4);
+
+        Predicate<Aluno> aprovado = a -> a.getNota() >= 7;
+        Function<Aluno, Double> apenasNota = a -> a.getNota();
+
+        BiFunction<Media, Double, Media> calcularMedia =
+                (media, nota) -> media.adicionar(nota);
+
+        listaAlunos.stream()
+                .filter(aprovado)
+                .map(apenasNota);
+
+
+    }
+
+}
