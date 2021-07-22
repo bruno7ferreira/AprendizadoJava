ab230cec0181af31b24cdb49b9fcdac555d0fc96
Bruno Rogério <b.rogerio2007@gmail.com>
2021-07-22 16:36:44 -0400

    Salvando aprendizado no Git

--- src/aprendizadoAvancadoJava/stream_api/match/Match.java
+++ src/aprendizadoAvancadoJava/stream_api/match/Match.java
@@ -4,6 +4,7 @@ import aprendizadoAvancadoJava.stream_api.Aluno;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Match {
 
@@ -15,6 +16,14 @@ public class Match {
         Aluno a4 = new Aluno("Kobe", 9.0);
 
         List<Aluno> listaAlunos = Arrays.asList(a1, a2, a3, a4);
+
+        Predicate<Aluno> aprovado = aluno -> aluno.getNota() >= 7;
+
+        //allMatch = todas condições verdadeiras para retorna true
+        System.out.println(listaAlunos.stream().allMatch(aprovado));
+
+        //anyMatch = pelo menos uma condição verdadeira já retorna true
+        System.out.println(listaAlunos.stream().anyMatch(aprovado));
 
     }
 }
