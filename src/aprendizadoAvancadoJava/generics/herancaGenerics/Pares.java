5ccccccb28b8a15e8f927f4a6d6fd5a8ec281817
bruno7ferreira <b.rogerio2007@gmail.com>
2021-07-28 14:39:56 -0400

    Atualização.

--- src/aprendizadoAvancadoJava/generics/herancaGenerics/Pares.java
+++ src/aprendizadoAvancadoJava/generics/herancaGenerics/Pares.java
@@ -5,8 +5,15 @@ import aprendizadoAvancadoJava.generics.Par;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-public class Pares<C, V> {
+public class Pares<C extends Number, V> {
 
     private final SortedSet<Par<C, V>> itens = new TreeSet<>();
+
+    public void adicionar(C chave, V valor) {
+        if (chave == null) return; //se a chave for nula, ele sai do método
+        itens.add(new Par<C, V>(chave, valor));
+
+    }
+
 
 }
