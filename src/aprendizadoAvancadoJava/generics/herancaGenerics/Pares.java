a717e2680f39396386aa87603a29399ace07d69e
bruno7ferreira <b.rogerio2007@gmail.com>
2021-07-28 14:49:08 -0400

    Atualização.

--- src/aprendizadoAvancadoJava/generics/herancaGenerics/Pares.java
+++ src/aprendizadoAvancadoJava/generics/herancaGenerics/Pares.java
@@ -2,6 +2,7 @@ package aprendizadoAvancadoJava.generics.herancaGenerics;
 
 import aprendizadoAvancadoJava.generics.Par;
 
+import java.util.Optional;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -13,6 +14,18 @@ public class Pares<C extends Number, V> {
         if (chave == null) return; //se a chave for nula, ele sai do método
         itens.add(new Par<C, V>(chave, valor));
 
+    }
+
+    //Pegando o valor da chave
+    public V getValor(C chave) {
+        if (chave == null) return null;
+
+        Optional<Par<C, V>> parOptional = itens.stream()
+                .filter(cvPar -> chave.equals(cvPar.getChave()))
+                .findFirst();
+
+        return parOptional.isPresent()
+                ? parOptional.get().getValor() : null;
     }
 
 
