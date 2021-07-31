7d9ba85adde9aff356eb240ce4ba176a6e9421cf
Bruno Rogério <b.rogerio2007@gmail.com>
2021-07-30 23:21:32 -0400

    Atualização.

--- src/aprendizadoAvancadoJava/tratamento_de_erro/excecao_personalizada/TesteValidacoes.java
+++ src/aprendizadoAvancadoJava/tratamento_de_erro/excecao_personalizada/TesteValidacoes.java
@@ -7,13 +7,13 @@ public class TesteValidacoes {
     public static void main(String[] args) {
 
         try {
-            Aluno aluno1 = new Aluno("Bruno", -7.0);
-            Validar.aluno(aluno1);
-        } catch (StringVaziaException e) {
+            Aluno aluno1 = new Aluno("Bruno", 7.0);
+            Validar.aluno(null);
+        } catch (StringVaziaException | NumeroForaIntervaloException | IllegalArgumentException e) {
             System.out.println("Mensagem de erro-> " + e.getMessage());
-        } catch (NumeroForaIntervaloException e) {
+        } /*catch (NumeroForaIntervaloException e) {
             System.out.println("Mensagem de erro-> " + e.getMessage());
-        }
+        }   e possível usar 2 ou mais catch em um try */
 
 
         System.out.println("Fim!");
