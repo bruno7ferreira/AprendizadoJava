33c922355ab63ee4ecb95f47a419c5ce07943269
Bruno Rogério <b.rogerio2007@gmail.com>
2021-08-13 19:25:05 -0400

    Aprendendo padrão observer

--- src/aprendizadoAvancadoJava/observer/Porteiro.java
+++ src/aprendizadoAvancadoJava/observer/Porteiro.java
@@ -2,10 +2,33 @@ package aprendizadoAvancadoJava.observer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Porteiro {
 
     private List<ObservadorChegadaAniversariante> observadores = new ArrayList<>();
     // lista com classes que implementam a interface
+
+    public void registrarObservadores(ObservadorChegadaAniversariante observador) {
+        observadores.add(observador);
+    }
+
+    public void monitorar() {
+        Scanner entrada = new Scanner(System.in);
+
+        String valor = "";
+
+        while (!"sair".equalsIgnoreCase(valor)) {
+            System.out.print("Aniversariante chegou? ");
+            valor = entrada.nextLine();
+
+            if ("sim".equalsIgnoreCase(valor)) {
+                //notificar os observadores
+            } else {
+                System.out.println("Alarme falso!");
+            }
+        }
+
+    }
 
 }
