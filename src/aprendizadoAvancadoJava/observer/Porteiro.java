b324eca067306b940613bfb4c7159c92e30d0ee7
Bruno Rogério <b.rogerio2007@gmail.com>
2021-08-13 19:16:57 -0400

    Aprendendo padrão observer

---
+++ src/aprendizadoAvancadoJava/observer/Porteiro.java
@@ -0,0 +1,11 @@
+package aprendizadoAvancadoJava.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Porteiro {
+
+    private List<ObservadorChegadaAniversariante> observadores = new ArrayList<>();
+    // lista com classes que implementam a interface
+
+}
